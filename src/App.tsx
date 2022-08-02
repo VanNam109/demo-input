02d2ac7d9e78a1c2ae4fcad79a356f7b9908b169
Van Nam <nguyennam0109198@gmail.com>
2022-08-02 16:01:34 +0700

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,15 @@ import "./App.css";
 function App() {
   return (
     <Box w="100vw" h="var(--app-height)">
-      <Box px="1.5rem" pb="5rem" overflow="auto" h="100%">
-        <Box>Input</Box>
-        <Box height="300px"></Box>
-        <Box width="100%" pt="1rem" pb="3.0rem">
-          <Box position="relative">
-            <Input
-              className="field"
-              p="0 1rem"
-              borderRadius="9px"
-              fontSize="1.2rem"
-              fontWeight="500"
-            />
-          </Box>
-        </Box>
-      </Box>
+      <Box>Input</Box>
+      <Box height="300px"></Box>
+      <Input
+        className="field"
+        p="0 1rem"
+        borderRadius="9px"
+        fontSize="1.2rem"
+        fontWeight="500"
+      />
     </Box>
   );
 }
